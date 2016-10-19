da3b8817628dc1e795859c07e7ec2f68f0f715ef
Aman Agrawal <aman_cse14@yahoo.com>
2016-10-19 10:31:07 +0100

    Added a test ASP.NET solution to show the usage of the plugin

---
+++ Leaflet.DriveTime/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="bootstrap.min.js" />
+/// <reference path="jquery-3.1.1.min.js" />
+/// <reference path="jquery-3.1.1.slim.min.js" />
+/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="../src/leaflet.drivetime.js" />
